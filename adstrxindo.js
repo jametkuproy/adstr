708f80ed84b88afcddd3ddaef3d2f4b8308cd364
jametkuproy <91093095+jametkuproy@users.noreply.github.com>
2021-09-21 03:12:29 +0700

    Create adstrxindo.js

---
+++ adstrxindo.js
@@ -0,0 +1,12 @@
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://www.performancetrustednetwork.com/z5gsy2eukf?key=22eddf143a168535bfffefbd0c581abc', '_blank');
+	});
+//]]>
