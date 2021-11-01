6aa9b2a9d64511c5128db8aea0a90287de407684
jametkuproy <91093095+jametkuproy@users.noreply.github.com>
2021-11-02 05:53:04 +0700

    Create ads2bannerkotak.js

---
+++ ads2bannerkotak.js
@@ -0,0 +1,10 @@
+&lt;script type=&quot;text/javascript&quot;&gt;
+	atOptions = {
+		&#039;key&#039; : &#039;4a188556aca1be95e08eca6154538d38&#039;,
+		&#039;format&#039; : &#039;iframe&#039;,
+		&#039;height&#039; : 250,
+		&#039;width&#039; : 300,
+		&#039;params&#039; : {}
+	};
+	document.write(&#039;&lt;scr&#039; + &#039;ipt type=&quot;text/javascript&quot; src=&quot;http&#039; + (location.protocol === &#039;https:&#039; ? &#039;s&#039; : &#039;&#039;) + &#039;://www.effectiveperformanceformat.com/4a188556aca1be95e08eca6154538d38/invoke.js&quot;&gt;&lt;/scr&#039; + &#039;ipt&gt;&#039;);
+&lt;/script&gt;
