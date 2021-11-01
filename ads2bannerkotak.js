c366b4fda4a60ccf7d6e0fa343a321b152f68b97
jametkuproy <91093095+jametkuproy@users.noreply.github.com>
2021-11-02 05:57:47 +0700

    Update ads2bannerkotak.js

--- ads2bannerkotak.js
+++ ads2bannerkotak.js
@@ -1,10 +1,10 @@
-&lt;script type=&quot;text/javascript&quot;&gt;
+<script type="text/javascript">
 	atOptions = {
-		&#039;key&#039; : &#039;4a188556aca1be95e08eca6154538d38&#039;,
-		&#039;format&#039; : &#039;iframe&#039;,
-		&#039;height&#039; : 250,
-		&#039;width&#039; : 300,
-		&#039;params&#039; : {}
+		'key' : '4a188556aca1be95e08eca6154538d38',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
 	};
-	document.write(&#039;&lt;scr&#039; + &#039;ipt type=&quot;text/javascript&quot; src=&quot;http&#039; + (location.protocol === &#039;https:&#039; ? &#039;s&#039; : &#039;&#039;) + &#039;://www.effectiveperformanceformat.com/4a188556aca1be95e08eca6154538d38/invoke.js&quot;&gt;&lt;/scr&#039; + &#039;ipt&gt;&#039;);
-&lt;/script&gt;
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectiveperformanceformat.com/4a188556aca1be95e08eca6154538d38/invoke.js"></scr' + 'ipt>');
+</script>
